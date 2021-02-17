e1cccc2d64072154f4dbef77b02ad4a3502555d5
daredyoshi <daredyoshi@gmail.com>
2021-02-17 16:11:28 +0000

    Added source from first HelloWindow lesson

---
+++ HelloWindow/src/Main.cpp
@@ -0,0 +1,90 @@
+// ALWAYS LOAD GLAD FIRST
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// these are callback functions that we are binding to glfw
+// they are defined after the main function, but declared here so we can bind them
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    glfwInit();
+    // Set glfw to use open gl version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create our window object 
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if ( window == NULL ){
+        std::cout << "Failed to create GLFW window" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    //the moment a user resizes the window the viewport should be 
+    //adjusted as well this is a callback to do that
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // initilize glad
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << '\n';
+        return -1;
+    }
+
+    // we don't want the application to draw a single image and then
+    // immediately quit and close the window. 
+    // this checks if the user has requested to close the window
+    // Each iteration of this loop is called a frame
+    while(!glfwWindowShouldClose(window)){
+        // check to see if the user has escaped
+        processInput(window);
+        // thi checks if any events are triggered (like keyboard/mouse)
+        glfwPollEvents();
+        // let's test our rendering by clearing the buffer (black)
+        // and adding our own color
+        // this is a state-setting function for glClear
+        glClearColor(0.2, 0.3, 0.3, 1.0);
+        // now clear with the color
+        // The possible bits we can set are GL_COLOR_BUFFER_BIT
+        // GL_DEPTH_BUFFER_BIT and GL_STENCIL_BUFFER_BIT
+        glClear(GL_COLOR_BUFFER_BIT);
+        // this will swap the color buffer that is used to render 
+        // and show the output on the screen
+        glfwSwapBuffers(window);
+    }
+
+        
+    // clean up/delete GLFW's resources that were allocated
+    glfwTerminate();
+    
+    
+    return 0;
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // because GLFW binds to this callback I can't change the inputs.
+    // I don't like piling on warnings so I'm putting this here to explicity
+    // tell the compiler I'm aware that window isn't use
+    (void)window;
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    // the bottom two numbers are where the window is located
+    glViewport(0, 0, width, height);
+}
+
+
