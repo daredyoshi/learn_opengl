7a17067e20c4ee0bd8c43c03547b09c0fec6fe40
daredyoshi <daredyoshi@gmail.com>
2021-03-11 12:24:11 +0000

    Added lesson on uniform buffer objects

---
+++ 021_Uniform_buffers/src/Camera.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/trigonometric.hpp"
+#include "glm/fwd.hpp"
+#include "glm/gtc/matrix_access.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#define GLM_SWIZZLE
+
+const float DEFAULT_YAW                 {   -90.0f           };
+const float DEFAULT_PITCH               {    0.0f            };
+const float DEFAULT_ROLL                 {    0.0f            };
+const float DEFAULT_MOVE_SPEED          {    4.5f            };
+const float DEFAULT_DIR_SPEED           {    70.0f           };
+const float DEFAULT_MOUSE_SENSITIVITY   {    0.1f            };
+const float DEFAULT_FOV                 {    45.0f           };
+const glm::vec3 DEFAULT_POS             { 0.0f, 0.0f, 3.0f   };
+const glm::vec3 DEFAULT_DIR             { 0.0f, 0.0f,-1.0f   };
+const glm::vec3 DEFAULT_UP              { 0.0f, 1.0f, 0.0f   };
+
+const glm::vec3 GLOBAL_UP               { 0.0f, 1.0f, 0.0f   };
+
+class Camera {
+private:
+    // the position of our camera
+    glm::vec3 m_pos;
+    // the direction fo our camera
+    // tecnically this is pointing in the reverse direction of what it is targeting
+    glm::vec3 m_dir;
+    // glm::vec3 camZ { glm::normalize(cameraPos - cameraTarget) };
+    // get the right access by crossing up with the direction
+    glm::vec3 m_up;
+    double m_yaw ;
+    double m_pitch;
+    double m_roll;
+    double m_moveSpeed;
+    double m_dirSpeed;
+    double m_mouseSensitivity;
+    double m_fov ;
+
+
+
+    // this is to check for if it's the starting cam
+    bool m_firstMouse { true };
+    double m_lastX{0};
+    double m_lastY{0};
+
+    double m_width { 800.0f };
+    double m_height { 600.0f };
+    double m_near { 0.1f };
+    double m_far { 100.0f };
+
+    void updateDirUp();
+
+public:
+
+    Camera( glm::vec3 pos=DEFAULT_POS, 
+            glm::vec3 dir=DEFAULT_DIR, 
+            glm::vec3 up=DEFAULT_UP, 
+            double yaw=DEFAULT_YAW, 
+            double pitch=DEFAULT_PITCH,
+            double roll=DEFAULT_ROLL,
+            double moveSpeed=DEFAULT_MOVE_SPEED, 
+            double dirSpeed=DEFAULT_DIR_SPEED,
+            double mouseSensitivity=DEFAULT_MOUSE_SENSITIVITY,
+            double fov=DEFAULT_FOV) 
+        : 
+            m_pos{pos}, 
+            m_dir{dir}, 
+            m_up{up}, 
+            m_yaw{yaw}, 
+            m_pitch{pitch}, 
+            m_roll{roll},
+            m_moveSpeed{moveSpeed}, 
+            m_dirSpeed{dirSpeed},
+            m_mouseSensitivity{mouseSensitivity}, 
+            m_fov{fov} 
+        {} 
+            
+    enum class CameraDirection {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        LEFT_ROLL,
+        RIGHT_ROLL
+    };
+
+    glm::mat4 getProjectionMatrix() const {
+        return glm::perspective(glm::radians(m_fov), m_width / m_height, m_near, m_far);
+    };
+    
+    glm::mat4 getViewMatrix() const {
+        return glm::lookAt(m_pos, m_pos + m_dir, m_up);
+    }
+
+    glm::vec3 getPos() const { return m_pos; }
+    glm::vec3 getDir() const { return m_dir; }
+
+    void changeFov(const float offset);
+    void setSize(const double width, const double height) { m_width = width; m_height = height; }
+    // set the direction based on an xposition and a yposition 
+    // (usually from mouse input)
+    void setDirection(const double xpos, const double ypos);
+    void incrementDirection(const CameraDirection direction, const float deltaTime);
+    void incrementPosition(const CameraDirection direction, const float deltaTime, const bool useGlobalUp=false);
+};
---
+++ 021_Uniform_buffers/src/Main.cpp
@@ -0,0 +1,317 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "ShaderProgram.h"
+#include "Camera.h"
+#include "Model.h"
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main(int numArgs, char *args[])
+{    
+
+    (void)numArgs; 
+        // Get the last position of '/'
+    std::string aux(args[0]);
+    // we aren't using numArgs
+
+    // get '/' or '\\' depending on unix/mac or windows.
+#if defined(_WIN32) || defined(WIN32)
+    int pos = aux.rfind('\\');
+#else
+    int pos = aux.rfind('/');
+#endif
+
+    // Get the path and the name
+    std::string CWD = aux.substr(0,pos+1);
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    std::string geometryShaderPath = CWD + "shaders/vert/geometry.glsl";
+    std::string redShaderPath = CWD + "shaders/frag/red.glsl";
+    std::string greenShaderPath = CWD + "/shaders/frag/green.glsl";
+    std::string blueShaderPath = CWD + "/shaders/frag/blue.glsl";
+    std::string yellowShaderPath = CWD + "/shaders/frag/yellow.glsl";
+    ShaderProgram shaderRed(geometryShaderPath, redShaderPath);
+    ShaderProgram shaderGreen(geometryShaderPath, greenShaderPath);
+    ShaderProgram shaderBlue(geometryShaderPath, blueShaderPath);
+    ShaderProgram shaderYellow(geometryShaderPath, yellowShaderPath);
+    
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float cubeVertices[] = {
+        // positions         
+        -0.5f, -0.5f, -0.5f, 
+         0.5f, -0.5f, -0.5f,  
+         0.5f,  0.5f, -0.5f,  
+         0.5f,  0.5f, -0.5f,  
+        -0.5f,  0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+
+        -0.5f, -0.5f,  0.5f, 
+         0.5f, -0.5f,  0.5f,  
+         0.5f,  0.5f,  0.5f,  
+         0.5f,  0.5f,  0.5f,  
+        -0.5f,  0.5f,  0.5f, 
+        -0.5f, -0.5f,  0.5f, 
+
+        -0.5f,  0.5f,  0.5f, 
+        -0.5f,  0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+        -0.5f, -0.5f,  0.5f, 
+        -0.5f,  0.5f,  0.5f, 
+
+         0.5f,  0.5f,  0.5f,  
+         0.5f,  0.5f, -0.5f,  
+         0.5f, -0.5f, -0.5f,  
+         0.5f, -0.5f, -0.5f,  
+         0.5f, -0.5f,  0.5f,  
+         0.5f,  0.5f,  0.5f,  
+
+        -0.5f, -0.5f, -0.5f, 
+         0.5f, -0.5f, -0.5f,  
+         0.5f, -0.5f,  0.5f,  
+         0.5f, -0.5f,  0.5f,  
+        -0.5f, -0.5f,  0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+
+        -0.5f,  0.5f, -0.5f, 
+         0.5f,  0.5f, -0.5f,  
+         0.5f,  0.5f,  0.5f,  
+         0.5f,  0.5f,  0.5f,  
+        -0.5f,  0.5f,  0.5f, 
+        -0.5f,  0.5f, -0.5f, 
+    };
+    // cube VAO
+    unsigned int cubeVAO, cubeVBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    // configure a uniform buffer object
+    // ---------------------------------
+    // first. We get the relevant block indices
+    unsigned int uniformBlockIndexRed = glGetUniformBlockIndex(shaderRed, "Matrices");
+    unsigned int uniformBlockIndexGreen = glGetUniformBlockIndex(shaderGreen, "Matrices");
+    unsigned int uniformBlockIndexBlue = glGetUniformBlockIndex(shaderBlue, "Matrices");
+    unsigned int uniformBlockIndexYellow = glGetUniformBlockIndex(shaderYellow, "Matrices");
+    // then we link each shader's uniform block to this uniform binding point
+    glUniformBlockBinding(shaderRed, uniformBlockIndexRed, 0);
+    glUniformBlockBinding(shaderGreen, uniformBlockIndexGreen, 0);
+    glUniformBlockBinding(shaderBlue, uniformBlockIndexBlue, 0);
+    glUniformBlockBinding(shaderYellow, uniformBlockIndexYellow, 0);
+    // Now actually create the buffer
+    unsigned int uboMatrices;
+    glGenBuffers(1, &uboMatrices);
+    glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+    glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(glm::mat4), NULL, GL_STATIC_DRAW);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    // define the range of the buffer that links to a uniform binding point
+    glBindBufferRange(GL_UNIFORM_BUFFER, 0, uboMatrices, 0, 2 * sizeof(glm::mat4));
+
+    // store the projection matrix (we only do this once now) (note: we're not using zoom anymore by changing the FoV)
+    glm::mat4 projection = glm::perspective(45.0f, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(projection));
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+  
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // set the view and projection matrix in the uniform block - we only have to do this once per loop iteration.
+        glm::mat4 view = camera.getViewMatrix();
+        glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
+        glBufferSubData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(view));
+        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+        // draw 4 cubes 
+        // RED
+        glBindVertexArray(cubeVAO);
+        shaderRed.use();
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-0.75f, 0.75f, 0.0f)); // move top-left
+        shaderRed.setMatrix("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // GREEN
+        shaderGreen.use();
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.75f, 0.75f, 0.0f)); // move top-right
+        shaderGreen.setMatrix("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // YELLOW
+        shaderYellow.use();
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-0.75f, -0.75f, 0.0f)); // move bottom-left
+        shaderYellow.setMatrix("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // BLUE
+        shaderBlue.use();
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.75f, -0.75f, 0.0f)); // move bottom-right
+        shaderBlue.setMatrix("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteBuffers(1, &cubeVBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if((glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)){
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE){
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+
+    // navigate with esdf
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::RIGHT, deltaTime);
+   
+    // navigate up and down with wr
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::DOWN, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+        camera.incrementPosition(Camera::CameraDirection::UP, deltaTime);
+   
+    // you can look around with ijkl
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        camera.incrementDirection(Camera::CameraDirection::DOWN, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        camera.incrementDirection(Camera::CameraDirection::UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) 
+        camera.incrementDirection(Camera::CameraDirection::LEFT, deltaTime); 
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) 
+        camera.incrementDirection(Camera::CameraDirection::RIGHT, deltaTime);
+    // roll with uo
+    
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+        camera.incrementDirection(Camera::CameraDirection::LEFT_ROLL, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+        camera.incrementDirection(Camera::CameraDirection::RIGHT_ROLL, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    (void)window;
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+    (void)window;
+    camera.setDirection(xpos, ypos);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+    (void)window;
+    (void)xoffset;
+    camera.changeFov(yoffset);        
+}
