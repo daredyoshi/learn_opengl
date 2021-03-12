ac8ef9e81e75e2ced1e4c241c8503d2baa926b3a
daredyoshi <daredyoshi@gmail.com>
2021-03-12 17:28:40 +0000

    Added lesson on instancing with models

---
+++ 026_AsteroidField/src/Mesh.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <vector>
+
+
+#include "Materials.h"
+#include "glm/glm.hpp"
+#include "ShaderProgram.h"
+#include "Materials.h"
+
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 Uv;
+};
+
+class Mesh{
+private:
+    unsigned int m_VAO;
+    unsigned int m_VBO;
+    unsigned int m_EBO;
+    std::vector<Vertex> m_vertices;
+    std::vector<unsigned int> m_indices;
+    Material m_mat;
+    void init();
+
+public:
+
+    Mesh(
+            std::vector<Vertex> vertices, 
+            std::vector<unsigned int> indices, 
+            Material mat
+        ) :
+            m_vertices{vertices},
+            m_indices{indices},
+            m_mat{mat} 
+        {
+            init();
+        }
+
+    unsigned int getVAO() const { return m_VAO; }
+    unsigned int getVBO() const { return m_VBO; }
+    void draw(ShaderProgram &shader) const;
+    Material& getMat() { return m_mat; };
+    std::vector<unsigned int>& getIndices() { return m_indices; }
+            
+};
