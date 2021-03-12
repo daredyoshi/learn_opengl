4bea4cf67a5f22b90202b9221638d1bcd01d2b26
daredyoshi <daredyoshi@gmail.com>
2021-03-12 10:43:34 +0000

    Added example for the drawing normals with the geometry shader

---
+++ 024_NormalsVisualizer/src/Model.h
@@ -0,0 +1,23 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.h"
+
+class Model{
+private:
+    std::vector<Mesh> m_meshes;
+    std::string m_directory;
+
+    void loadModel(std::string_view path);
+    void processNode(const aiNode *node, const aiScene *scene);
+    Mesh processMesh(const aiMesh *mesh, const aiScene *scene);
+    Texture loadMaterialTextures(aiMaterial *mat, 
+            aiTextureType type, unsigned int idx);
+
+public:
+    Model(std::string_view modelFile){
+        loadModel(modelFile);
+    }
+    void draw(ShaderProgram &shader) const;
+};
